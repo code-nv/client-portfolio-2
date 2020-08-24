cdb9208243a5c63925ce63d4ebc7e40703aa5977
Nick Reyno <nickrenoe@gmail.com>
2020-08-23 21:35:22 -0400

    mobile nav tweaks

--- themes/luke-fair/functions.php
+++ themes/luke-fair/functions.php
@@ -7,7 +7,7 @@ function luke_fair_styles()
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
     wp_enqueue_script('main_scripts', get_theme_file_uri('/js/script.js'), null, '1.0', true);
     // wp_enqueue_style('styles', get_theme_file_uri().'/css/styles.css');
-    wp_enqueue_style('style', get_template_directory_uri() . '/css/styles.css?ver=1.0.1', null, '1.0.0', 'all');
+    wp_enqueue_style('style', get_template_directory_uri() . '/css/styles.css?ver=1.1.2', null, '1.0.0', 'all');
 };
 
 add_action('wp_enqueue_scripts', 'luke_fair_styles');
